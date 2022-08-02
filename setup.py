a7a43f9c70e170df32bcc134cf6ff0c1822db34b
joaquinbogado <joaquinbogado@gmail.com>
2022-08-02 14:34:15 +0200

    Fixed another broken version string

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/stratosphereips/zeeklog2pandas',
-    version='version='0.1.1'',
+    version='0.1.1',
     zip_safe=False,
 )
